b49531d7a1413572d370ac712502d1172a8ab8cd
Sandy J <j.sandy.jenkins@gmail.com>
2021-03-15 12:07:06 +0100

    Database Entry?

---
+++ views/pages/addToDatabase.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+function addEntry() {
+
+    var rowsnumber = pool.query(
+        "SELECT COUNT(*) FROM test_table",
+        (err, res) => {
+        console.log(err, res);
+        pool.end();
+        }
+    );
+
+    pool.query(
+        "INSERT INTO test_table values (" + rowsnumber + ", 'dummy')",
+        (err, res) => {
+        console.log(err, res);
+        pool.end();
+        }
+    );
+
+};

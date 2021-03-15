4f5abdac730ada7f45fd317e7942dfb30b94328c
Sandy J <j.sandy.jenkins@gmail.com>
2021-03-15 09:45:18 +0100

    database test
    lets go

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
 express()
   .use(express.static("pages"))
   .get("/", function (req, res) {

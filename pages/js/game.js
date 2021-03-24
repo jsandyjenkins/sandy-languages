b5e434f842eb3cfd6dfbd33ab80ca0a39eda3f87
Sandy J <j.sandy.jenkins@gmail.com>
2021-03-24 14:45:29 +0100

    past simple/past perfect
    a whole new tense!

--- pages/js/game.js
+++ pages/js/game.js
@@ -1,6 +1,7 @@
 var input = document.getElementById("wordInput");
 input.placeholder = "Enter here";
-var currentWord = Math.floor(Math.random() * 94);
+var currentWord = Math.round(Math.random() * 94);
+var tense = Math.round(Math.random());
 const myHeading = document.querySelector('h1');
 var obj;
 
@@ -9,27 +10,67 @@ fetch('./js/json/irreg_verbs.json')
     .then((out) => {
         console.log('Output: ', out);
 		obj=out;
-		myHeading.textContent = obj.word_list[currentWord].infinitive;
+		if (tense == 0) {
+			myHeading.textContent = "Past Simple: " + obj.word_list[currentWord].infinitive;
+		} else {
+			myHeading.textContent = "Past Perfect: " + obj.word_list[currentWord].infinitive;
+		}
 }).catch(err => console.error(err));
 
 input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
 	if (event.keyCode === 13) {
 		event.preventDefault();
-		if (input.value === obj.word_list[currentWord].past_simple) {
-			currentWord = Math.floor(Math.random() * 94);
-			myHeading.textContent = obj.word_list[currentWord].infinitive;
-			input.value = "";
+		if (tense == 0){
+			if (input.value === obj.word_list[currentWord].past_simple) {
+				currentWord = Math.floor(Math.random() * 94);
+				tense = Math.round(Math.random());
+				if (tense == 0) {
+					myHeading.textContent = "Past Simple: " + obj.word_list[currentWord].infinitive;
+				} else {
+					myHeading.textContent = "Past Perfect: " + obj.word_list[currentWord].infinitive;
+				}
+				input.value = "";
+			}
+		}else{
+			if (input.value === obj.word_list[currentWord].past_participle) {
+				currentWord = Math.floor(Math.random() * 94);
+				tense = Math.round(Math.random());
+				if (tense == 0) {
+					myHeading.textContent = "Past Simple: " + obj.word_list[currentWord].infinitive;
+				} else {
+					myHeading.textContent = "Past Perfect: " + obj.word_list[currentWord].infinitive;
+				}
+				input.value = "";
+			}
 		}
 	}
 });
 
 document.getElementById("myBtn").addEventListener("click", function() {
 	event.preventDefault();
-	if (input.value === obj.word_list[currentWord].past_simple) {
-		currentWord = Math.floor(Math.random() * 94);
-		myHeading.textContent = obj.word_list[currentWord].infinitive;
-		input.value = "";
+	if (tense == 0){
+		if (input.value === obj.word_list[currentWord].past_simple) {
+			currentWord = Math.floor(Math.random() * 94);
+			tense = Math.round(Math.random());
+			if (tense == 0) {
+				myHeading.textContent = "Past Simple: " + obj.word_list[currentWord].infinitive;
+			} else {
+				myHeading.textContent = "Past Perfect: " + obj.word_list[currentWord].infinitive;
+			}
+			input.value = "";
+		}
+	}else{
+		if (input.value === obj.word_list[currentWord].past_participle) {
+			currentWord = Math.floor(Math.random() * 94);
+			tense = Math.round(Math.random());
+			if (tense == 0) {
+				myHeading.textContent = "Past Simple: " + obj.word_list[currentWord].infinitive;
+			} else {
+				myHeading.textContent = "Past Perfect: " + obj.word_list[currentWord].infinitive;
+			}
+			input.value = "";
+		}
 	}
 });
 

de58c420d43b700af61cdbe251b806347c0c58c7
Sandy J <j.sandy.jenkins@gmail.com>
2021-03-24 14:02:48 +0100

    0.1 Alpha
    Verbs Game Active!
    Still not brilliant

--- pages/js/game.js
+++ pages/js/game.js
@@ -1,31 +1,24 @@
 var input = document.getElementById("wordInput");
 input.placeholder = "Enter here";
-var currentWord = Math.floor(Math.random() * 10);
+var currentWord = Math.floor(Math.random() * 94);
 const myHeading = document.querySelector('h1');
+var obj;
 
-var data = '{ "translations" : ['+
-'{ "id" : "1", "bulgarian" : "Kak si?", "english" : "How are you?" },'+
-'{ "id" : "2", "bulgarian" : "Dobre sam, blagodarja, A ti?", "english" : "Fine thanks, how are you?" },'+
-'{ "id" : "3", "bulgarian" : "Ne sam te vishdal mnogo vreme", "english" : "Long time, no see" },'+
-'{ "id" : "4", "bulgarian" : "Kak se kazvash?", "english" : "Whats your name?" },'+
-'{ "id" : "5", "bulgarian" : "Kazvam se", "english" : "My name is" },'+
-'{ "id" : "6", "bulgarian" : "Otkade si?", "english" : "Where are you from?" },'+
-'{ "id" : "7", "bulgarian" : "Az sam ot", "english" : "Im from" },'+
-'{ "id" : "8", "bulgarian" : "Nazdrave!", "english" : "Cheers!" },'+
-'{ "id" : "9", "bulgarian" : "Ne razbiram", "english" : "I dont understand" },'+
-'{ "id" : "10", "bulgarian" : "Sashaljavam", "english" : "Sorry" }]}'
-
-obj = JSON.parse(data);
-
-myHeading.textContent = obj.translations[currentWord].bulgarian;
+fetch('./js/json/irreg_verbs.json')
+    .then(res => res.json())
+    .then((out) => {
+        console.log('Output: ', out);
+		obj=out;
+		myHeading.textContent = obj.word_list[currentWord].infinitive;
+}).catch(err => console.error(err));
 
 input.addEventListener("keyup", function(event) {
   // Number 13 is the "Enter" key on the keyboard
 	if (event.keyCode === 13) {
 		event.preventDefault();
-		if (input.value === obj.translations[currentWord].english) {
-			currentWord = Math.floor(Math.random() * 10)
-			myHeading.textContent = obj.translations[currentWord].bulgarian;
+		if (input.value === obj.word_list[currentWord].past_simple) {
+			currentWord = Math.floor(Math.random() * 94);
+			myHeading.textContent = obj.word_list[currentWord].infinitive;
 			input.value = "";
 		}
 	}
